fd5b2aeef94728ed3d45d5b7abdadb4cb15d66a4
Syed Amear <Syed Amear@LAPTOP-VHSAH8S1>
2022-09-10 14:37:27 +0530

    Added Demo and Demo2

---
+++ src/test/java/Prcatice1/Demo2Test.java
@@ -0,0 +1,9 @@
+package Prcatice1;
+
+public class Demo2Test {
+	public static void main(String[] args) {
+		String str="how are you";
+		System.out.println(str);
+	}
+
+}
---
+++ src/test/java/Prcatice1/DemoTest.java
@@ -0,0 +1,8 @@
+package Prcatice1;
+
+public class DemoTest {
+public static void main(String[] args) {
+	String s="Automation";
+	System.out.println(s);
+}
+}
